c10791da3f6ec7abb732e2b8d30eb3348c74ab1e
Timothy Liao <tim80411@oneclass.tw>
2024-09-29 20:43:34 +0800

    feat: build AddTodoForm component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import AddTodoForm from '@/components/todo/AddTodoForm'
 import TodoList from '@/components/todo/TodoList'
 
 export default function Home() {
   return (
     <main className="container mx-auto p-4">
       <h1 className="text-2xl font-bold mb-4">To-Do List</h1>
+      <AddTodoForm />
       <TodoList />
     </main>
   )
--- src/components/todo/AddTodoForm.tsx
+++ src/components/todo/AddTodoForm.tsx
@@ -1,5 +1,31 @@
+'use client'
+import { useRef } from 'react';
+
 export default function AddTodoForm() {
+  const formRef = useRef<HTMLFormElement>(null);
+  const handleSubmit = async (formData: FormData) => {
+    console.log(formData)
+    // TODO: serer action
+    formRef.current?.reset();
+  }
+
   return (
-    <></>
-  )
+    <form 
+      ref={formRef}
+      className="flex mb-4"
+      action={handleSubmit}
+    >
+      <input
+        type="text"
+        placeholder="Add a new task..."
+        className="flex-grow shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+      />
+      <button
+        type="submit"
+        className="ml-2 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+      >
+        Add
+      </button>
+    </form>
+  );
 }
