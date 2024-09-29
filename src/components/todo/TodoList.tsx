134b23bbbf4a3f571a89a64a0e5183a9597d7f74
Timothy Liao <tim80411@oneclass.tw>
2024-09-29 22:43:30 +0800

    feat: add paginaition

--- src/components/todo/TodoList.tsx
+++ src/components/todo/TodoList.tsx
@@ -1,10 +1,27 @@
+'use client'
+import React, { useState } from 'react';
 import TodoItem from './TodoItem';
+import { Pagination } from '@mui/material';
 
 export default function TodoList() {
+  const [page, setPage] = useState(1);
+
+  const handlePageChange = (event: React.ChangeEvent<unknown>, value: number) => {
+    console.log('page change', value);
+    setPage(value);
+  };
+
   return (
     <div className="w-full bg-white">
-      <TodoItem/>
-      <TodoItem/>
+      <TodoItem />
+      <TodoItem />
+      <Pagination
+        count={10}
+        page={page}
+        onChange={handlePageChange}
+        color="primary"
+        className="mt-4 flex justify-center"
+      />
     </div>
   );
 }
